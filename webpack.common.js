8e21b918cec724f2bf9347ccbffa2570c00c5d66
AruSeito <AruSeito@gmail.com>
2021-02-09 20:05:18 +0800

    chore: 将webpack配置分为prod与dev

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,9 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/app.tsx',
-  mode: 'development',
-  devtool: 'inline-source-map',
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   },
@@ -24,7 +21,6 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // 在开发过程中回退到 style-loader
           process.env.NODE_ENV !== "prod"
             ? "style-loader"
             : MiniCssExtractPlugin.loader,
@@ -46,18 +42,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: '首页',
       template: 'src/index.html'
-    }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
     })
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    hot: true,
-    inline: true,
-    port: 18080,
-  }
+  ]
 
 }
---
+++ webpack.prod.js
@@ -0,0 +1,17 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+const common = require('./webpack.common');
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'inline-source-map',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new CleanWebpackPlugin()
+  ]
+})
