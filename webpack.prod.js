dcc8a770b14412abbb1db03b71b0ab849a11b030
AruSeito <AruSeito@gmail.com>
2021-02-09 20:07:08 +0800

    chore: 生产环境下devtool使用source-map

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   plugins: [
     new MiniCssExtractPlugin({
       filename: "[name].css",
